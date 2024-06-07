d5ad3eae40a5b1aae3b980efc4ab70ccfcbbdcec
Ziah <122858207+Zainap16@users.noreply.github.com>
2024-06-07 15:49:38 +0200

    yes

---
+++ Dicee+Challenge+-+Starting+Files/index.js
@@ -0,0 +1,14 @@
+var randomNumber1 = Math.floor(Math.random() * 6);
+var img = ["dice1.png","dice1.png","","",""]
+// console.log(randomNumber1);
+
+var dice1 = document.querySelector("img1");
+var dice2 = document.querySelector("img2");
+
+for (var i=0; i<=5;i++){
+    dice1.setAttribute("src", )
+}
+
+
+
+
