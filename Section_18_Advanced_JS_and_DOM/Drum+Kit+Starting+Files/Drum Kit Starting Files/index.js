4a2d25b7151c8cc5fbae73faa31162734ad8f7cf
Ziah <122858207+Zainap16@users.noreply.github.com>
2024-06-10 11:13:30 +0200

    to be continued

---
+++ Section_18_Advanced_JS_and_DOM/Drum+Kit+Starting+Files/Drum Kit Starting Files/index.js
@@ -0,0 +1,17 @@
+var numOfDrumBtn = document.querySelectorAll(".drum").length;
+
+// var audio = new Audio("./sounds/tom-1.mp3");
+var audio = ["./sounds/tom-1.mp3","./sounds/tom-2.mp3","./sounds/tom-3.mp3","./sounds/tom-4.mp3","./sounds/crash.mp3","./sounds/snare.mp3","./sounds/kick-bass.mp3"];
+
+
+
+for (var i = 0; i < numOfDrumBtn; i++ ){
+
+    document.querySelectorAll(".drum")[i].addEventListener("click",
+        function(){
+            
+        }
+    );
+
+}
+
