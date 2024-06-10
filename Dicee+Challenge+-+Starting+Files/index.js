698bee39b199674856a535ed0dc15e8252b24eaa
Ziah <122858207+Zainap16@users.noreply.github.com>
2024-06-10 09:50:58 +0200

    compleetd boss level was super easy!

--- Dicee+Challenge+-+Starting+Files/index.js
+++ Dicee+Challenge+-+Starting+Files/index.js
@@ -1,13 +1,30 @@
 var randomNumber1 = Math.floor(Math.random() * 6);
-var img = ["dice1.png","dice1.png","","",""]
-// console.log(randomNumber1);
+var randomNumber2 = Math.floor(Math.random() * 6);
 
-var dice1 = document.querySelector("img1");
-var dice2 = document.querySelector("img2");
+var img = ["./images/dice1.png","./images/dice2.png","./images/dice3.png","./images/dice4.png","./images/dice5.png","./images/dice6.png"]
+console.log(img[randomNumber1]);
 
-for (var i=0; i<=5;i++){
-    dice1.setAttribute("src", )
-}
+var dice1 = document.querySelector(".img1");
+var dice2 = document.querySelector(".img2");
+
+var newImg1 = img[randomNumber1];
+var newImg2 = img[randomNumber2];
+
+var heading = document.querySelector("h1");
+
+function myFunction(){
+    dice1.setAttribute("src", newImg1);
+    dice2.setAttribute("src", newImg2);
+
+    if (randomNumber1 > randomNumber2){
+        heading.innerHTML = "Player 1 Wins!"
+    } else if (randomNumber2 > randomNumber1 ){
+         heading.innerHTML = "Player 2 Wins!"
+    }else{
+         heading.innerHTML = "Draw!"
+    }
+} 
+
 
 
 
