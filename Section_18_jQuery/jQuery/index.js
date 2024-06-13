b8acc6f456017badfccda937a8d2d8b63c9f48da
Ziah <122858207+Zainap16@users.noreply.github.com>
2024-06-13 15:56:01 +0200

    incomepleted mini challenage

--- Section_18_jQuery/jQuery/index.js
+++ Section_18_jQuery/jQuery/index.js
@@ -1,1 +1,8 @@
-$("h1").css("color","red");
+// $("h1").addClass("big-title");
+
+// when you click on button change it to that key
+// need keydown
+
+$("body").keydown(function(event){
+   =(event.key) ;
+})
