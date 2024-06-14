0a486d49f9a6bad7fa0670f8e2e2b5d811d7a451
Ziah <122858207+Zainap16@users.noreply.github.com>
2024-06-14 09:31:59 +0200

    ys

--- Section_18_jQuery/jQuery/index.js
+++ Section_18_jQuery/jQuery/index.js
@@ -3,6 +3,28 @@
 // when you click on button change it to that key
 // need keydown
 
-$("body").keydown(function(event){
-   =(event.key) ;
-})
+// $("document").keydown(function(event){
+//    $("h1").text(event.key);
+// });
+
+$("h1").on("mouseover", function(){
+   $("h1").css("color","blue");
+   $("h1").css("font-size","5rem");
+
+});
+
+$("h1").on("mouseout", function(){
+   $("h1").css("color","black");
+   
+});
+
+
+
+
+
+
+
+
+
+
+
