e1497d100d7ef9d43dec0d3b4ca99a0844f78da0
Ziah <122858207+Zainap16@users.noreply.github.com>
2024-09-04 15:56:32 +0200

    yws

--- Section_18_jQuery/jQuery/index.js
+++ Section_18_jQuery/jQuery/index.js
@@ -1,13 +1,5 @@
-$("h1").on("mouseover", function(){
-   $("h1").css("color","blue");
-   $("h1").css("font-size","5rem");
-
-});
-
-$("h1").on("mouseout", function(){
-   $("h1").css("color","black");
-   $("h1").css('font-size','2rem');
-   
+$(document).on('keypress',function(event){
+   $('h1').text(event.key);
 });
 
 
