e638575c32068fec81535a122d0b0d04460ccac1
Ziah <122858207+Zainap16@users.noreply.github.com>
2024-09-04 15:26:41 +0200

    yes

--- Section_18_jQuery/jQuery/index.js
+++ Section_18_jQuery/jQuery/index.js
@@ -1,12 +1,3 @@
-// $("h1").addClass("big-title");
-
-// when you click on button change it to that key
-// need keydown
-
-// $("document").keydown(function(event){
-//    $("h1").text(event.key);
-// });
-
 $("h1").on("mouseover", function(){
    $("h1").css("color","blue");
    $("h1").css("font-size","5rem");
@@ -15,6 +6,7 @@ $("h1").on("mouseover", function(){
 
 $("h1").on("mouseout", function(){
    $("h1").css("color","black");
+   $("h1").css('font-size','2rem');
    
 });
 
