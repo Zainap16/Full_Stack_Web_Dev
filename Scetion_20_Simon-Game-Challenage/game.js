00300edb14c31eb1d7062c4e5661003c6e3b28f2
Zainap16 <zvanblerck1@gmail.com>
2024-09-05 11:39:33 +0200

    added files

---
+++ Scetion_20_Simon-Game-Challenage/game.js
@@ -0,0 +1,11 @@
+// alert('test');
+
+let buttonsColours = ["red", "blue", "green", "yellow"];
+
+let randomChosenColour = buttonsColours[randomNumber];
+function nextSequence(){
+    let randomNumber = Math.floor(Math.random() * 4);
+    return randomNumber;
+}
+
+// let randomChosenColour = buttonsColours[nextSequence()];
