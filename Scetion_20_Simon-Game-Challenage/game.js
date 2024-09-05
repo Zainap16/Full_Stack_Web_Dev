27602617c0a55179c4247e974d1345329c094819
Zainap16 <zvanblerck1@gmail.com>
2024-09-05 12:56:00 +0200

    i give up

--- Scetion_20_Simon-Game-Challenage/game.js
+++ Scetion_20_Simon-Game-Challenage/game.js
@@ -1,11 +1,18 @@
 // alert('test');
-
+let gamePattern = [];
 let buttonsColours = ["red", "blue", "green", "yellow"];
 
-let randomChosenColour = buttonsColours[randomNumber];
-function nextSequence(){
-    let randomNumber = Math.floor(Math.random() * 4);
-    return randomNumber;
-}
+function nextSequence() {
 
-// let randomChosenColour = buttonsColours[nextSequence()];
+    var randomNumber = Math.floor(Math.random() * 4);
+    var randomChosenColour = buttonColours[randomNumber];
+    gamePattern.push(randomChosenColour);
+  
+    //1. Use jQuery to select the button with the same id as the randomChosenColour
+    //2. Use Google/Stackoverflow to figure out how you can use jQuery to animate a flash to the button selected in step 1.
+    $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+  
+    //3. Use Google/Stackoverflow to figure out how you can use Javascript to play the sound for the button colour selected in step 1.
+    var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+    audio.play();
+  }
